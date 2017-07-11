fae73f0531682c35ba1f1ad8767dca4dc3566bdc
Gaidinlung Kamei <gaidink@mba14.local>
2017-07-11 23:54:07 +0530

    some little changes

--- static/script/main.js
+++ static/script/main.js
@@ -5,28 +5,27 @@
  */
 
 
-
-function convert(cgpa) {
-    if (cgpa < 7) {
-        var percentage = 7.1 * cgpa + 12;
-    } else if (cgpa >= 7) {
-        var percentage = 7.4 * cgpa + 12;
+function convert(cgpi) {
+    if (cgpi < 7) {
+        var percentage = 7.1 * cgpi + 12;
+    } else if (cgpi >= 7) {
+        var percentage = 7.4 * cgpi + 12;
     }
     return percentage;
 }
 
 function result_display() {
-    var cgpa = document.getElementById("cgpa").value;
-    cgpa = cgpa * 1;
+    var cgpi = document.getElementById("cgpi").value;
+    cgpi = cgpi * 1;
     //document.getElementById("msg").innerHTML = typeof cgpa;
-    if (typeof cgpa != 'number') {
+    if (typeof cgpi != 'number') {
         alert("Enter numeral input");
-    } else if (!(cgpa >= 0)) {
+    } else if (!(cgpi >= 0)) {
         alert("Input should be greater than or equal to 0");
-    } else if (!(cgpa <= 10)) {
+    } else if (!(cgpi <= 10)) {
         alert("Input should be less than or equal to 10");
     } else {
         document.getElementById("msg").innerHTML = "Percentage is ";
-        document.getElementById("result").innerHTML = convert(cgpa);
+        document.getElementById("result").innerHTML = convert(cgpi);
     }
 }
